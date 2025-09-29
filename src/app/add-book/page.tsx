4fc3f65059b2913a6f667dd8da87627a7f21a1eb
Tanyo Nikolov <tanyo@Tanyos-MacBook-Pro-2.local>
2025-09-29 18:39:58 +0300

    style: add book form styling

--- src/app/add-book/page.tsx
+++ src/app/add-book/page.tsx
@@ -1,44 +1,47 @@
 "use client";
 
 import { addBook } from "@/actions/books/actions";
+import PageHeading from "@/components/common/PageHeading";
 import Link from "next/link";
 import { useActionState } from "react";
 
 export default function AddBookPage() {
   const [state, formAction, pending] = useActionState(addBook, {});
   return (
-    <section>
-      <h1>Add book</h1>
-      <form action={formAction}>
-        <div>
+    <section className="flex flex-col space-around items-center">
+      <PageHeading text="Add a book" />
+      <form
+        action={formAction}
+        className="flex flex-col text-center px-4 w-[400px]"
+      >
+        <div className="my-2">
           <input
             name="title"
             placeholder="Book title..."
-            className="border p-2"
+            className="block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm outline-none ring-0 transition focus:border-indigo-500"
           />
           {state.errors?.title && (
             <p className="text-red-600 text-sm">{state.errors.title[0]}</p>
           )}
         </div>
 
-        <input
-          name="description"
-          placeholder="Book description..."
-          className="border p-2"
-        />
-
-        <div>
-          <input name="author" placeholder="Author" className="border p-2" />
+        <div className="my-2">
+          <input
+            name="author"
+            placeholder="Author"
+            className="block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm outline-none ring-0 transition focus:border-indigo-500"
+          />
           {state.errors?.author && (
             <p className="text-red-600 text-sm">{state.errors.author[0]}</p>
           )}
         </div>
 
-        <div>
+        <div className="my-2">
           <input
             name="publishDate"
+            type="date"
             placeholder="Book's publication date..."
-            className="border p-2"
+            className="block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm outline-none ring-0 transition focus:border-indigo-500"
           />
           {state.errors?.publishDate && (
             <p className="text-red-600 text-sm">
@@ -47,8 +50,19 @@ export default function AddBookPage() {
           )}
         </div>
 
-        <div>
-          <select name="readingStatus" className="border p-2">
+        <div className="my-2">
+          <input
+            name="description"
+            placeholder="Book description..."
+            className="block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm outline-none ring-0 transition focus:border-indigo-500"
+          />
+        </div>
+
+        <div className="my-2">
+          <select
+            name="readingStatus"
+            className="block w-full rounded-lg border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm outline-none ring-0 transition focus:border-indigo-500"
+          >
             <option value="" disabled={true}>
               Please, tell us if you are reading this book or already have done
               so
@@ -63,9 +77,11 @@ export default function AddBookPage() {
           )}
         </div>
 
-        <button type="submit" disabled={pending}>
-          {pending ? "Adding..." : "Add book"}
-        </button>
+        <div>
+          <button type="submit" disabled={pending} className="w-[200px] xl p-2 my-4 rounded cursor-pointer border border-gray-300 hover:bg-gray-100">
+            {pending ? "Adding..." : "Add book"}
+          </button>
+        </div>
 
         {state.ok && (
           <div>
